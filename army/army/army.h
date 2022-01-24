6736d6b499e2237f00ca4ff9fad633304d86e82d
Игорь Бондаренко <Bond_fh54@student.itstep.org>
2022-01-24 17:09:12 +0200

    fights

--- army/army/army.h
+++ army/army/army.h
@@ -56,19 +56,18 @@ public:
 			this->head->sold->attack(otherArmy->head->sold);
 			std::cout << "ATTACKER " << this->head->sold->getSoldierInfo() << " hits " << otherArmy->head->sold->getSoldierInfo() << "\n";
 			if (otherArmy->head->sold->getIsAlive() == false) otherArmy->remove();
+			if (otherArmy->head == nullptr)
+			{
+				std::cout << "attacker won\n";
+				return;
+			}
 			//defender's turn
 			otherArmy->head->sold->attack(this->head->sold);
 			std::cout << "DEFENDER " << otherArmy->head->sold->getSoldierInfo() << " hits " << this->head->sold->getSoldierInfo() << "\n";
 			if (this->head->sold->getIsAlive() == false) this->remove();
-			//check if any army won
 			if (this->head == nullptr)
 			{
 				std::cout << "defender won\n";
-				return;
-			}
-			if (otherArmy->head == nullptr)
-			{
-				std::cout << "attacker won\n";
 				return;
 			}
 		}
